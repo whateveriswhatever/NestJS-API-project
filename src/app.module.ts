1124b564e3a1f1d58220aed7e423c1019473c8d9
whoever-you-wanna-think <banvietthenaocungdc@gmail.com>
2023-09-15 17:33:19 +0700

    First commit with building Auth Service

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, UserModule, BookmarkModule],
+})
+export class AppModule {}
